561321c595b583a7331745a038b8383f86c3eda5
liubo817 <lyubomir.y.stanev.2019@elsys-bg.org>
2022-03-11 16:21:18 +0200

    Update SpaceVisionCoreModule.ino

--- SpaceVision/SpaceVisionCoreModule/SpaceVisionCoreModule.ino
+++ SpaceVision/SpaceVisionCoreModule/SpaceVisionCoreModule.ino
@@ -6,18 +6,30 @@ MPU6050 mpu6050(Wire);
 int backBuzzerPin = 9;
 int rightBuzzerPin = 6;
 int leftBuzzerPin = 5;
+int buttonPin = 1;
+int buttonState = 0;
 void setup() {
   Serial.begin(9600);
-  Wire.begin();
-  mpu6050.begin();
+  //Wire.begin();
+  //mpu6050.begin();
   mpu6050.calcGyroOffsets(true);
   pinMode(backBuzzerPin, OUTPUT);
   pinMode(rightBuzzerPin, OUTPUT);
   pinMode(leftBuzzerPin, OUTPUT); 
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
   mpu6050.update();
+
+  //button code
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH)
+  {
+    Wire.begin();
+    mpu6050.begin();
+  }
+  
   Serial.print("angleX : ");
   Serial.print(mpu6050.getAngleX());
   Serial.print("\tangleY : ");
