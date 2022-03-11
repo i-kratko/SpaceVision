463d7cb743da861885e25a695c2b5c8dcccd4916
liubo817 <lyubomir.y.stanev.2019@elsys-bg.org>
2022-03-11 15:02:59 +0200

    backbuzzer

--- SpaceVision/SpaceVisionCoreModule/SpaceVisionCoreModule.ino
+++ SpaceVision/SpaceVisionCoreModule/SpaceVisionCoreModule.ino
@@ -3,12 +3,13 @@
 #include <Wire.h>
 
 MPU6050 mpu6050(Wire);
-
+int backBuzzerPin = 9;
 void setup() {
   Serial.begin(9600);
   Wire.begin();
   mpu6050.begin();
   mpu6050.calcGyroOffsets(true);
+  pinMode(backBuzzerPin, OUTPUT);
 }
 
 void loop() {
@@ -19,4 +20,16 @@ void loop() {
   Serial.print(mpu6050.getAngleY());
   Serial.print("\tangleZ : ");
   Serial.println(mpu6050.getAngleZ());
+  if (mpu6050.getAngleY()<-40)
+  {
+      tone(backBuzzerPin, 100);
+  }
+  else if (mpu6050.getAngleY()>20)
+  {
+      tone(backBuzzerPin, 100);
+  }
+  else 
+  {
+      noTone(backBuzzerPin);
+  }
 }
