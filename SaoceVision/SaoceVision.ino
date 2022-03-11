e9983d67836ff4c1611abe82bf5de9d3733ff31e
liubo817 <lyubomir.y.stanev.2019@elsys-bg.org>
2022-03-11 11:34:18 +0200

    Update SaoceVision.ino

--- SaoceVision/SaoceVision.ino
+++ SaoceVision/SaoceVision.ino
@@ -1,9 +1,20 @@
+int trigPin = 9;
+int echoPin = 10;
+int distance;
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT); 
+  Serial.begin(9600); 
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop() 
+{
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.017;
+  Serial.print("Distance: ");
+  Serial.println(distance);
 }
