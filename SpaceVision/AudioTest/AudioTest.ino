e35a7663744e8f1e2e1609d7a18f0bf71ce3845f
i-kratko <66026762+i-kratko@users.noreply.github.com>
2022-03-12 19:38:00 +0200

    Update AudioTest.ino

--- SpaceVision/AudioTest/AudioTest.ino
+++ SpaceVision/AudioTest/AudioTest.ino
@@ -2,8 +2,8 @@
 #include "DFRobotDFPlayerMini.h"
 
 // Use pins 2 and 3 to communicate with DFPlayer Mini
-static const uint8_t PIN_MP3_TX = 2; // Connects to module's RX 
-static const uint8_t PIN_MP3_RX = 3; // Connects to module's TX 
+static const uint8_t PIN_MP3_TX = 11; // Connects to module's RX 
+static const uint8_t PIN_MP3_RX = 10; // Connects to module's TX 
 SoftwareSerial softwareSerial(PIN_MP3_RX, PIN_MP3_TX);
 
 // Create the Player object
